9f275595e9fcc9af04a021b47a52b8e70d160629
MalonzaElkanah <elkanahmalonza@gmail.com>
2022-04-12 17:10:19 +0300

    Update to-do lists

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-# to-do
+# to-do App
 
+## Operations
+- create a todo list
+- update a todo list
+- view todo lists
+- archive a todo list
+- delete a todo list
+
+- add items to a todo list
+- delete items from a todo list
+- check items from a todo list
+
+## Requirements
+- python 3.8
+- flask 1.0
+- sqlite 
+- 
+
+## Run following Commands in shell to run the app.
 export FLASK_APP=flaskr
 export FLASK_ENV=development
 flask run
--- flaskr/todo.py
+++ flaskr/todo.py
@@ -16,9 +16,18 @@ def index():
     todos = db.execute(
         'SELECT t.id, name, description, created, status'
         ' FROM todo t JOIN user u ON t.user_id = u.id'
+        ' WHERE t.status = "ACTIVE"'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('todo/index.html', todos=todos)
+
+    todo_items = []
+    for todo in todos:
+        per = get_percentage_complete(int(todo['id']))
+        todo = dict(todo)
+        todo.update({'complete': per})
+        todo_items.append(todo)
+
+    return render_template('todo/index.html', todos=todo_items)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
@@ -48,31 +57,53 @@ def create():
 
 
 def get_todo(id, check_user=True):
+    # Get todo list
     todo = get_db().execute(
-        'SELECT t.id, t.name, t.description, t.created, t.status'
+        'SELECT t.id, t.user_id, t.name, t.description, t.created, t.status'
         ' FROM todo t JOIN user u ON t.user_id = u.id'
         ' WHERE t.id = ?',
         (id,)
     ).fetchone()
 
-    lists = None
+    # Check if List Exist and belongs to request Owner
     if todo is None:
         abort(404, f"Post id {id} doesn't exist.")
-    else:
-        lists = get_db().execute(
-            'SELECT l.id, l.name, l.created, l.status'
-            ' FROM list l JOIN todo t ON l.todo_id = t.id'
-            ' WHERE t.id = ?',
-            (id,)
-        ).fetchall()
-
-    if check_user and id != g.user['id']:
+    elif check_user and todo['user_id'] != g.user['id']:
         abort(403)
+
+    # Get todo List Items
+    lists = None
+    lists = get_db().execute(
+        'SELECT l.id, l.name, l.created, l.status'
+        ' FROM list l JOIN todo t ON l.todo_id = t.id'
+        ' WHERE t.id = ?',
+        (id,)
+    ).fetchall()
+
+    # Check if list is completed
+    count = 0
+    for item in lists:
+        if item["status"] == "NOT_DONE":
+            count += 1
+            break
+            
+    if todo['status'] != "ACTIVE" and count > 0:
+        # Set List Active 
+        change_todo_status(id, 'ACTIVE')
+        todo = dict(todo)
+        todo.update({'status': 'ACTIVE'}) # todo['status'] = "ACTIVE"
+    elif todo['status'] not in ["COMPLETE", "ARCHIVE"] and count == 0:
+        # Set List Complete
+        change_todo_status(id, 'COMPLETE')
+        todo = dict(todo)
+        todo.update({'status': 'COMPLETE'}) # ['status'] = "COMPLETE"
+
+
 
     return {'todo': todo, 'lists': lists}
 
 
-@bp.route('/<int:id>/detail', methods=('GET', 'POST'))
+@bp.route('/<int:id>/list', methods=('GET', 'POST'))
 @login_required
 def detail(id):
     if request.method == 'POST':
@@ -92,6 +123,27 @@ def detail(id):
     return render_template('todo/detail.html', todo=todo['todo'], lists=todo['lists'])
 
 
+@bp.route('/<int:id>/list/<int:list_id>/delete')
+@login_required
+def delete_list(id, list_id):
+    todo = get_todo(id)['todo']
+    db = get_db()
+    db.execute('DELETE FROM list WHERE id = ?', (list_id,))
+    db.commit()
+    flash('List Item Deleted.')
+    return redirect(url_for('todo.detail', id=id))
+
+
+@bp.route('/<int:id>/list/<int:list_id>/check')
+@login_required
+def check_list(id, list_id):
+    get_todo(id)
+    db = get_db()
+    db.execute('UPDATE list SET status = ? WHERE id = ?', ('COMPLETE', list_id))
+    db.commit()
+    flash('List Item Checked.')
+    return redirect(url_for('todo.detail', id=id))
+
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
@@ -104,7 +156,7 @@ def update(id):
         error = None
 
         if not name:
-            error = 'Title is required.'
+            error = 'Name is required.'
 
         if error is not None:
             flash(error)
@@ -116,9 +168,10 @@ def update(id):
                 (name, description, id)
             )
             db.commit()
-            return redirect(url_for('todo.index'))
+            flash("{} List updated".formart(name))
+            return redirect(url_for('todo.detail', id=id))
 
-    return render_template('todo/update.html', post=post)
+    return render_template('todo/update.html', todo=todo)
 
 
 @bp.route('/<int:id>/delete', methods=('POST',))
@@ -129,3 +182,60 @@ def delete(id):
     db.execute('DELETE FROM todo WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('todo.index'))
+
+
+@bp.route('/<int:id>/archive')
+@login_required
+def archive(id):
+    # get todo data
+    todo = get_todo(id)["todo"]
+    # check if list is completed
+    if todo["status"] == 'COMPLETE':
+        db = get_db()
+        db.execute('UPDATE todo SET status = ? WHERE id = ?', ('ARCHIVE', id))
+        db.commit()
+        flash('List Archived.')
+    else:
+        flash('Error: Some Items in the list are unchecked')
+    return redirect(url_for('todo.detail', id=id))
+
+
+@bp.route('/archives')
+@login_required
+def archives():
+    db = get_db()
+    todos = db.execute(
+        'SELECT t.id, name, description, created, status'
+        ' FROM todo t JOIN user u ON t.user_id = u.id'
+        ' WHERE t.status = "ARCHIVE"'
+        ' ORDER BY created DESC'
+    ).fetchall()
+
+    todo_items = []
+    for todo in todos:
+        per = get_percentage_complete(int(todo['id']))
+        todo = dict(todo)
+        todo.update({'complete': per})
+        todo_items.append(todo)
+
+    return render_template('todo/index.html', todos=todo_items, status="ARCHIVE")
+
+
+def change_todo_status(id, status):
+    db = get_db()
+    db.execute('UPDATE todo SET status = ? WHERE id = ?', (status, id))
+    db.commit()
+
+
+def get_percentage_complete(id):
+    lists = get_todo(id)['lists']
+    count = 0
+    for item in lists:
+        if item['status'] == 'COMPLETE':
+            count += 1
+
+    # calcuate percentage complete
+    if count > 0:
+        complete = (count/len(lists)) * 100 
+        return int(complete)
+    return count
